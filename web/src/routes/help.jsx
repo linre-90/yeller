16d40921d366717655528c24d3c2b21840e57a3c
linre-90 <juho.lindemark@gmail.com>
2024-02-21 11:28:08 +0200

    Update help.jsx

--- web/src/routes/help.jsx
+++ web/src/routes/help.jsx
@@ -1,12 +1,30 @@
+import { createUseStyles } from "react-jss"
+
+const styles = createUseStyles({
+    helpPage: {
+    "& p": {
+        width: "100%",
+        paddingTop: "20px",
+        textAlign: "center",
+        color: "white",
+        margin: "auto"
+    }}
+});
+
+
 /**
  * Render help pages.
  * @returns 
  */
 export const Help = () => {
+    const classes = styles();
     return(
-        <div>
-            <p>Help section about Yeller</p>
+        <div className={classes.helpPage}>
             <p>No this does not provide mental help for you weirdos... This is about Yeller app.</p>
+            <p>1. If there is problem clear browser history...</p>
+            <p>2. If there is problem clear browser history...</p>
+            <p>3. If there is problem clear browser history...</p>
+            <p>Last resort. Check mirror...</p>
         </div>
     )
 }
